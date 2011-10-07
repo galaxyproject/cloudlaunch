9b2c809a5b13565a3f8b6b28a28a9f2d98e9614d
chapmanb <chapmanb@50mail.com>
2011-10-07 16:20:22 -0400

    Simple entry form

--- biocloudcentral/settings.py
+++ biocloudcentral/settings.py
@@ -1,4 +1,5 @@
 # Django settings for biocloudcentral project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -106,6 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.basename(__file__), os.pardir, "templates"),
 )
 
 INSTALLED_APPS = (
--- biocloudcentral/views.py
+++ biocloudcentral/views.py
@@ -1,6 +1,23 @@
 """Base views.
 """
 from django.http import HttpResponse
+from django import forms
+from django.shortcuts import render_to_response
+
+class CloudManForm(forms.Form):
+    """Details needed to boot a setup and boot a CloudMan instance.
+    """
+    cluster_name = forms.CharField(required=True)
+    password = forms.CharField(label=_(u'Password'),
+                               widget=forms.PasswordInput(render_value=False))
+    access_key = forms.CharField(required=True)
+    secret_key = forms.CharField(required=True)
 
 def home(request):
-    return HttpResponse("BioCloudCentral")
+    if require.method == "POST":
+        form = CloudManForm(require.POST)
+        if form.is_valid():
+            return HttpResponse("Sweet")
+    else:
+        form = CloudManForm()
+    return render_to_response("home.html", {"form": form})
