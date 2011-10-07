4d6f33732d25ba9e894c1d8568359642e3a57776
chapmanb <chapmanb@50mail.com>
2011-10-07 15:43:46 -0400

    Initial configuration

--- biocloudcentral/urls.py
+++ biocloudcentral/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
