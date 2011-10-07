dae4ff2252863d7b4f2b22d87c574898a8705ae6
chapmanb <chapmanb@50mail.com>
2011-10-07 16:21:17 -0400

    Fix password entry

--- biocloudcentral/views.py
+++ biocloudcentral/views.py
@@ -8,8 +8,7 @@ class CloudManForm(forms.Form):
     """Details needed to boot a setup and boot a CloudMan instance.
     """
     cluster_name = forms.CharField(required=True)
-    password = forms.CharField(label=_(u'Password'),
-                               widget=forms.PasswordInput(render_value=False))
+    password = forms.CharField(widget=forms.PasswordInput(render_value=False))
     access_key = forms.CharField(required=True)
     secret_key = forms.CharField(required=True)
 
