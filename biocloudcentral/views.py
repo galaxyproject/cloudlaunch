f18c33c822c3f943c72f1e1ec8163844557d3a50
chapmanb <chapmanb@50mail.com>
2011-10-10 22:12:17 -0400

    Pretty up the inputs page

--- biocloudcentral/views.py
+++ biocloudcentral/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django import forms
 from django.shortcuts import render, redirect
 
+# ## Landing page with redirects
+
 def home(request):
     launch_url = request.build_absolute_uri("/launch")
     if launch_url.startswith(("http://127.0.0.1", "http://localhost")):
@@ -11,15 +13,27 @@ def home(request):
     else:
         return redirect("https://biocloudcentral.herokuapp.com/launch")
 
-# -- cloudman configuration entry details
+# ## CloudMan launch and configuration entry details
 
 class CloudManForm(forms.Form):
     """Details needed to boot a setup and boot a CloudMan instance.
     """
-    cluster_name = forms.CharField(required=True)
-    password = forms.CharField(widget=forms.PasswordInput(render_value=False))
-    access_key = forms.CharField(required=True)
-    secret_key = forms.CharField(required=True)
+    cluster_name = forms.CharField(required=True,
+                                   help_text="Name of your cluster used for identification. "
+                                   "This can be any name you choose.")
+    password = forms.CharField(widget=forms.PasswordInput(render_value=False),
+                               help_text="Password used to access the CloudMan web interface "
+                               "and your instance via ssh and FreeNX.")
+    access_key = forms.CharField(required=True,
+                                 help_text="Your Amazon Access Key ID. Available from "
+                                 "the security credentials page.")
+    secret_key = forms.CharField(required=True,
+                                 help_text="Your Amazon Secret Access Key. Also available "
+                                 "from the security credentials page.")
+    instance_type = forms.ChoiceField((("m1.large", "Large"),
+                                       ("t1.micro", "Micro"),
+                                       ("m1.xlarge", "Extra Large")),
+                                      help_text="Amazon instance type to start.")
 
 def launch(request):
     """Configure and launch CloudBioLinux and CloudMan servers.
