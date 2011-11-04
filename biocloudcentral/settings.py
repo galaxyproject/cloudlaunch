572a9c5062619be9ef9519940ec0a381cc7711d3
chapmanb <chapmanb@50mail.com>
2011-11-03 20:15:52 -0400

    Add static content to processors for pulling in css and javascript

--- biocloudcentral/settings.py
+++ biocloudcentral/settings.py
@@ -106,6 +106,10 @@ TEMPLATE_LOADERS = (
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request",
 )
 
