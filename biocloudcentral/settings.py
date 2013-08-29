793c1b91722c9c3adeace3cd2f5814cb10c86d0e
Enis Afgan <afgane@gmail.com>
2013-08-29 08:19:10 +0200

    Update settings.py with Heroku recommendations: https://devcenter.heroku.com/articles/django

--- biocloudcentral/settings.py
+++ biocloudcentral/settings.py
@@ -30,6 +30,12 @@ MANAGERS = ADMINS
 # }
 DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
