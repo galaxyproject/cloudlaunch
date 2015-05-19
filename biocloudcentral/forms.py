983db2ce226e138c4074dac6146124c3ed5a5d90
Enis Afgan <afgane@gmail.com>
2015-05-18 23:30:15 -0400

    Make the email field optional and move it to the bottom of the form

--- biocloudcentral/forms.py
+++ biocloudcentral/forms.py
@@ -36,9 +36,6 @@ class CloudManForm(forms.Form):
                                  help_text="Your cloud account secret key. For the Amazon cloud, also available "
                                  "from the <a href='{0}' {1} tabindex='-1'>security credentials page</a>."
                                  .format(key_url, target))
-    institutional_email = forms.EmailField(required=True,
-                                 widget=forms.TextInput(attrs={"class": "%s" % textbox_size}),
-                                 help_text="Your institutional email. For grant-reporting purposes only.")
     # A simple text input element
     # cluster_name = forms.CharField(required=True,
     #                                help_text="Name of your cluster used for identification and "
@@ -115,6 +112,9 @@ class CloudManForm(forms.Form):
                               widget=forms.TextInput(attrs={"class": textbox_size}),
                               help_text="Having selected 'Custom image' in the previous drop down,"
                               "provide desired maching image ID (e.g., ami-da5532cs)")
+    institutional_email = forms.EmailField(required=False,
+                                 widget=forms.TextInput(attrs={"class": "%s" % textbox_size}),
+                                 help_text="Your institutional email. For grant-reporting purposes only.")
 
 class FlavorAdminForm(forms.ModelForm):
     class Meta:
