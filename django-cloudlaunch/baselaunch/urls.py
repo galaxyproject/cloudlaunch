c5032f3bcb1946fe7beba0adc57d8f00cf8a9944
Enis Afgan <afgane@gmail.com>
2016-01-04 15:48:12 -0500

    Remove integration of drf-nested-routers from a1277ded790c06128d25c50fbaf435df747c070a

--- django-cloudlaunch/baselaunch/urls.py
+++ django-cloudlaunch/baselaunch/urls.py
@@ -17,45 +17,39 @@ Including another URLconf
 from django.conf.urls import include
 from django.conf.urls import url
 # from django.contrib import admin
-# from rest_auth import views as rest_auth_views
-# from rest_auth.registration import views as rest_reg_views
+from rest_auth import views as rest_auth_views
+from rest_auth.registration import views as rest_reg_views
 # from rest_framework import viewsets
 from baselaunch import views
-# from .util import HybridRouter
-from rest_framework_nested import routers
+from .util import HybridRouter
 
 
-router = routers.SimpleRouter()
-# router = HybridRouter()
+router = HybridRouter()
 router.register(r'applications', views.ApplicationViewSet)
 router.register(r'images', views.ImageViewSet)
 router.register(r'infrastructure', views.InfrastructureViewSet)
-# Nested routes
-# app_router = routers.NestedSimpleRouter(router, r'applications', lookup='application')
-# app_router.register(r'categories', views.CategoryViewSet, base_name='app-categories')
 # django rest-auth
-# router.register(r'auth/login', rest_auth_views.LoginView,
-#                 base_name='rest_login')
-# router.register(r'auth/logout', rest_auth_views.LogoutView,
-#                 base_name='rest_logout')
-# router.register(r'auth/user', rest_auth_views.UserDetailsView,
-#                 base_name='rest_user_details')
-# router.register(r'auth/password/reset', rest_auth_views.PasswordResetView,
-#                 base_name='rest_password_reset')
-# router.register(r'auth/password/reset/confirm',
-#                 rest_auth_views.PasswordResetConfirmView,
-#                 base_name='test')
-# router.register(r'auth/password/change', rest_auth_views.PasswordChangeView,
-#                 base_name='rest_password_change')
-# # django rest-auth registration views
-# router.register(r'auth/registration', rest_reg_views.RegisterView,
-#                 base_name='rest_register')
-# router.register(r'auth/registration/verify-email', rest_reg_views.VerifyEmailView,
-#                 base_name='rest_verify_email')
+router.register(r'auth/login', rest_auth_views.LoginView,
+                base_name='rest_login')
+router.register(r'auth/logout', rest_auth_views.LogoutView,
+                base_name='rest_logout')
+router.register(r'auth/user', rest_auth_views.UserDetailsView,
+                base_name='rest_user_details')
+router.register(r'auth/password/reset', rest_auth_views.PasswordResetView,
+                base_name='rest_password_reset')
+router.register(r'auth/password/reset/confirm',
+                rest_auth_views.PasswordResetConfirmView,
+                base_name='test')
+router.register(r'auth/password/change', rest_auth_views.PasswordChangeView,
+                base_name='rest_password_change')
+# django rest-auth registration views
+router.register(r'auth/registration', rest_reg_views.RegisterView,
+                base_name='rest_register')
+router.register(r'auth/registration/verify-email', rest_reg_views.VerifyEmailView,
+                base_name='rest_verify_email')
 
 urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
-    # url(r'^api/v1/', include(app_router.urls)),
-    # url(r'^api/v1/auth/', include('rest_framework.urls',
-    #                               namespace='rest_framework')),
+    url(r'^api/v1/auth/', include('rest_framework.urls',
+                                  namespace='rest_framework')),
 ]
