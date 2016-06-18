4367b7459247f2b129cd41c6a161f806ca76f728
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-06-19 01:44:56 +0530

    Added security group rule handling.

--- django-cloudlaunch/baselaunch/backend_plugins/app_plugin.py
+++ django-cloudlaunch/baselaunch/backend_plugins/app_plugin.py
@@ -4,33 +4,68 @@ import yaml
 from baselaunch import domain_model
 
 
-def _get_or_create_key_pair(provider, kp_name):
-    """
-    If a key pair with the provided ``kp_name`` does not exist, create it.
-    """
-    kp = provider.security.key_pairs.find(name=kp_name)
-    if kp:
-        kp = kp[0]
-    else:
-        kp = provider.security.key_pairs.create(name=kp_name)
-    return kp
-
 class BaseAppPlugin():
 
-    @abc.abstractstaticmethod    
+    @abc.abstractstaticmethod
     def process_config_data(cloud_version_config, data):
         pass
 
-    def launch_app(self, credentials, cloud, version, cloud_version_config, cloudlaunch_config, user_data):
+    def _get_or_create_key_sg(self, provider, sg_name, description):
+        """
+        If a key pair with the provided ``kp_name`` does not exist, create it.
+        """
+        sg = provider.security.security_groups.find(name=sg_name)
+        if sg:
+            return sg[0]
+        else:
+            return provider.security.security_groups.create(name=sg_name,
+                                                            description=description)
+
+    def apply_app_firewall_settings(self, provider, firewall_settings):
+        """
+        Apply any firewall settings defined for the app in cloudlaunch
+        settings. The following format is expected:
+
+        "firewall": [
+            {
+                "rules": [
+                    {
+                        "from": "1",
+                        "to": "65535",
+                        "src_group": "CloudMan",
+                        "cidr": "0.0.0.0/0",
+                        "protocol": "tcp"
+                    }
+                ],
+                "securityGroup": "CloudMan"
+            }
+        ]
+        """
+        for group in firewall_settings:
+            sg_name = group.get('securityGroup') or 'CloudLaunchDefault'
+            sg_desc = group.get('description') or 'Created by CloudLaunch'
+            sg = self._get_or_create_key_sg(provider, sg_name, sg_desc)
+            for rule in group.get('rules', []):
+                sg.add_rule(ip_protocol=rule.get('protocol'),
+                            from_port=rule.get('from'),
+                            to_port=rule.get('to'),
+                            cidr_ip=rule.get('cidr'))
+                            #src_group=rule.get('src_group'))
+
+    def launch_app(self, credentials, cloud, version,
+                   cloud_version_config, app_config, user_data):
+        cloudlaunch_config = app_config.get("config_cloudlaunch", {})
+        print(cloudlaunch_config)
         provider = domain_model.get_cloud_provider(cloud, credentials)
-        kp = _get_or_create_key_pair(provider, cloudlaunch_config.get(
-            'keyPair', 'cloudlaunch_key_pair'))
-        sg = provider.security.security_groups.find('CloudMan')[0]
         img = provider.compute.images.get(cloud_version_config.image.image_id)
+        kp = provider.security.key_pairs.create(
+            name=cloudlaunch_config.get('keyPair') or 'cloudlaunch_key_pair')
+        self.apply_app_firewall_settings(provider, cloudlaunch_config.get('firewall', []))
+
         it = cloudlaunch_config.get(
             'instanceType', cloud_version_config.default_instance_type)
         inst_type = provider.compute.instance_types.get(it)
-    
+
         ud = yaml.dump(user_data, default_flow_style=False, allow_unicode=False)
         print("Launching an instance type %s, KP %s, with ud: %s" %
               (inst_type, kp, ud))
