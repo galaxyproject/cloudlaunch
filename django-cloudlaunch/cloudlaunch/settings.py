3bf37e88d4a73e320a6a5cf03b861a6eac686b59
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-06-26 02:13:30 +0530

    Fixed missing import.

--- django-cloudlaunch/cloudlaunch/settings.py
+++ django-cloudlaunch/cloudlaunch/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
-
+import manage
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
