3a97f1a3099a75f4f35c77e1cb07573fedd3db7f
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-06-26 06:12:52 +0530

    Change database to postgres

--- django-cloudlaunch/cloudlaunch/settings.py
+++ django-cloudlaunch/cloudlaunch/settings.py
@@ -108,10 +108,22 @@ WSGI_APPLICATION = 'cloudlaunch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'cloudlaunch',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'cloudlaunch',
+        'PASSWORD': 'CHANGEMEONINSTALL',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
