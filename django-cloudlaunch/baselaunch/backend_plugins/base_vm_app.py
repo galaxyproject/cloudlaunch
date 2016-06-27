8813f93d335f92524968046b7d8ed8de8d0b0c5d
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-06-28 03:36:32 +0530

    Added base vm app

---
+++ django-cloudlaunch/baselaunch/backend_plugins/base_vm_app.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ValidationError
+from .app_plugin import BaseAppPlugin
+
+class BaseVMAppPlugin(BaseAppPlugin):
+
+    @staticmethod
+    def process_app_config(name, cloud_version_config, credentials, app_config):
+        return {}
+
+    def launch_app(self, task, name, cloud_version_config, credentials, app_config, user_data):
+        result = super(BaseVMAppPlugin, self).launch_app(task, name, cloud_version_config, credentials, app_config, user_data)
+        result['cloudLaunch']['applicationURL'] = '{0}'.format(result['cloudLaunch']['publicIP'])
+        return result
