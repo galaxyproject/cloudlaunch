22b67d3864291617a2f2778e002a14ffc1a27229
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-06-27 19:16:47 +0530

    Make celery use json for results

--- django-cloudlaunch/cloudlaunch/celeryconfig.py
+++ django-cloudlaunch/cloudlaunch/celeryconfig.py
@@ -2,3 +2,6 @@ BROKER_URL = 'django://'
 #BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+#CELERY_ACCEPT_CONTENT = ['json', 'yaml']
