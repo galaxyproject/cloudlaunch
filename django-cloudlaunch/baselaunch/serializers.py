eeb575c5eac391c2074a8fed71a42f05bb45957a
Nuwan Goonasekera <nuwan.ag@gmail.com>
2017-01-10 23:33:55 +0530

    Added extra_data field to Cloud model

--- django-cloudlaunch/baselaunch/serializers.py
+++ django-cloudlaunch/baselaunch/serializers.py
@@ -469,6 +469,7 @@ class CloudSerializer(serializers.ModelSerializer):
     region_name = serializers.SerializerMethodField()
 
     cloud_type = serializers.SerializerMethodField()
+    extra_data = serializers.SerializerMethodField()
 
     def get_region_name(self, obj):
         if hasattr(obj, 'aws'):
@@ -485,6 +486,34 @@ class CloudSerializer(serializers.ModelSerializer):
             return 'openstack';
         else:
             return 'unknown';
+        
+    def get_extra_data(self, obj):
+        if hasattr(obj, 'aws'):
+            aws = obj.aws
+            extra_data = {}
+            if aws.compute:
+                compute = aws.compute
+                extra_data['ec2_region_name'] = compute.ec2_region_name
+                extra_data['ec2_region_endpoint'] = compute.ec2_region_endpoint
+                extra_data['ec2_conn_path'] = compute.ec2_conn_path
+                extra_data['ec2_port'] = compute.ec2_port
+                extra_data['ec2_is_secure'] = compute.ec2_is_secure
+            if aws.object_store:
+                s3 = aws.object_store
+                extra_data['s3_host'] = s3.s3_host
+                extra_data['s3_conn_path'] = s3.s3_conn_path
+                extra_data['s3_port'] = s3.s3_port
+                extra_data['s3_is_secure'] = s3.s3_is_secure
+            return extra_data
+        elif hasattr(obj, 'openstack'):
+            os = obj.openstack
+            return {
+                'auth_url': os.auth_url,
+                'region_name': os.region_name,
+                'identity_api_version': os.identity_api_version
+            }
+        else:
+            return {}
 
     class Meta:
         model = models.Cloud
