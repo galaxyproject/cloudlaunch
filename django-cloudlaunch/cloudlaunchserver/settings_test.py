9c033188ea6c23ac0c7fd5f51290c86bc9752a8a
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-04-10 21:10:45 +0530

    Move SIGINT handling to test settings

--- django-cloudlaunch/cloudlaunchserver/settings_test.py
+++ django-cloudlaunch/cloudlaunchserver/settings_test.py
@@ -1,7 +1,18 @@
 """
 Django settings used during cloudlaunch testing
 """
+import signal
+
+# The integration test script sends a SIGINT to terminate the django server
+# after the tests are complete. Handle the SIGINT here and terminate
+# gracefully, or coverage will terminate abruptly without writing the
+# .coverage file
+def test_signal_handler(*args, **kwargs):
+    sys.exit(0)
+signal.signal(signal.SIGINT, test_signal_handler)
+
 from cloudlaunchserver.settings import *  # noqa
+
 
 DATABASES = {
     'default': {
--- django-cloudlaunch/manage.py
+++ django-cloudlaunch/manage.py
@@ -3,14 +3,6 @@ import os
 import sys
 import signal
 
-# The integration test script sends a SIGINT to terminate the django server
-# after the tests are complete. Handle the SIGINT here and terminate
-# gracefully, or coverage will terminate abruptly without writing the
-# .coverage file
-def test_signal_handler(*args, **kwargs):
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, test_signal_handler)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloudlaunchserver.settings")
