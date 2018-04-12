53535606126601604507e9845002b6406fd80a67
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-04-12 21:25:00 +0530

    Fixed config settings and typo for celery test config

--- django-cloudlaunch/cloudlaunchserver/celeryconfig_test.py
+++ django-cloudlaunch/cloudlaunchserver/celeryconfig_test.py
@@ -2,8 +2,8 @@
 Celery settings used during cloudlaunch testing
 """
 broker_backend = 'memory'
-always_eager = True
-eaer_propagates_exceptions = True
+task_always_eager = True
+eager_propagates_exceptions = True
 result_serializer = 'json'
 task_serializer = 'json'
 accept_content = ['json']
