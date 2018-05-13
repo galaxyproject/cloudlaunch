ac00949f0324c699fa4a8b48dea3f186af6f8a9b
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-05-13 20:23:33 +0530

    Switched celery to separate test db

--- django-cloudlaunch/cloudlaunchserver/celeryconfig_test.py
+++ django-cloudlaunch/cloudlaunchserver/celeryconfig_test.py
@@ -4,7 +4,7 @@ Celery settings used during cloudlaunch testing
 broker_url = 'memory://'
 broker_transport_options = {'polling_interval': .01}
 broker_backend = 'memory'
-result_backend = 'django-db'
+result_backend = 'db+sqlite:///results.db'
 result_serializer = 'json'
 task_serializer = 'json'
 accept_content = ['json']
