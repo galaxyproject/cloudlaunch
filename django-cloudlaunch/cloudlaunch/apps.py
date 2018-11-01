d16d6f9feea1c3ab83d876bf175f8ef1508127bf
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-11-01 18:32:45 +0530

    Moved profile signal connection to djcloudbridge

--- django-cloudlaunch/cloudlaunch/apps.py
+++ django-cloudlaunch/cloudlaunch/apps.py
@@ -5,11 +5,3 @@ class CloudLaunchConfig(AppConfig):
 
     def ready(self):
         import cloudlaunch.signals  # noqa
-        from django.contrib.auth.models import User  # noqa
-        from django.contrib.auth.signals import user_logged_in  # noqa
-        from django.db.models.signals import post_save  # noqa
-
-        from djcloudbridge.signals import create_profile_at_login  # noqa
-        from djcloudbridge.signals import create_profile_with_user  # noqa
-        user_logged_in.connect(create_profile_at_login)
-        post_save.connect(create_profile_with_user, sender=User)
