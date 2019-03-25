11d39352168c3a46c5df21cb882b566a3eeb2c2a
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-03-25 21:10:22 +0530

    Improved error handling in init container script

--- django-cloudlaunch/scripts/init_container.sh
+++ django-cloudlaunch/scripts/init_container.sh
@@ -1,11 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 cd "${0%/*}"/..
 
 echo "Apply database migrations from `pwd`"
 python manage.py migrate
+# Save last error
+errcode=$?
+if [[ $errcode -eq 0 ]]; then
+    echo "Migrations successfully applied"
+else
+    echo "Failed to apply migrations. Quitting..."
+    exit $errcode
+fi
 
 echo "Load initial data from /app/initial_data/*.json"
 python manage.py loaddata /app/initial_data/*.json
 
+
 echo "Create a superuser"
-cat scripts/create_superuser.py | python manage.py shell
+cat /app/scripts/create_superuser.py | python manage.py shell
+errcode=$?
+if [[ $errcode -eq 0 ]]; then
+    echo "Successfully created the superuser"
+else
+    echo "Error creating superuser. Quitting..."
+    exit $errcode
+fi
