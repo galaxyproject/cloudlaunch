e769239fd4e2c459098eef56a93286094f783e14
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-03-26 15:19:45 +0530

    Read keys from env to support docker

--- django-cloudlaunch/cloudlaunchserver/settings_prod.py
+++ django-cloudlaunch/cloudlaunchserver/settings_prod.py
@@ -1,7 +1,6 @@
 """
 Django settings used during cloudlaunch production
 """
-
 from cloudlaunchserver.settings import *  # noqa
 
 DATABASES = {
@@ -14,3 +13,15 @@ DATABASES = {
         'PASSWORD': os.environ.get('CLOUDLAUNCH_DB_PASSWORD'),
     }
 }
+
+SECRET_KEY = os.environ.get('CLOUDLAUNCH_SECRET_KEY') or SECRET_KEY
+
+# Read fernet keys from env or default to existing settings keys
+ENV_FERNET_KEYS = os.environ.get('CLOUDLAUNCH_FERNET_KEYS')
+if ENV_FERNET_KEYS:
+    ENV_FERNET_KEYS = ENV_FERNET_KEYS.split(",")
+try:
+    # Use FERNET_KEYS defined in cloudlaunchserver.settings if defined
+    FERNET_KEYS = ENV_FERNET_KEYS or FERNET_KEYS
+except NameError:
+    FERNET_KEYS = ENV_FERNET_KEYS
