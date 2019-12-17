bc415fda5a1942cbbf79fbd75e59f183a98f6c7e
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-12-17 19:31:18 +0530

    Added app data import command

---
+++ django-cloudlaunch/cloudlaunch/management/commands/import_app_data.py
@@ -0,0 +1,46 @@
+import argparse
+import requests
+import yaml
+
+from django.core.management.base import BaseCommand
+
+import cloudlaunch.management.commands.serializers as mgmt_serializers
+
+
+class Command(BaseCommand):
+    help = 'Imports Application Data from a given url or file and updates' \
+           ' existing models'
+
+    def file_from_url(url):
+        """
+        Reads a file from a url
+        :return: content of file
+        """
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+            return r.text
+        except requests.exceptions.RequestException as e:
+            raise argparse.ArgumentTypeError(e)
+
+    def add_arguments(self, parser):
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument('-f', '--file', type=argparse.FileType('r'))
+        group.add_argument('-u', '--url', type=Command.file_from_url)
+
+    def handle(self, *args, **options):
+        if options['file']:
+            content = options['file'].read()
+        else:
+            content = options['url']
+
+        return self.import_app_data(yaml.safe_load(content))
+
+    @staticmethod
+    def import_app_data(yaml_data):
+        serializer = mgmt_serializers.ApplicationSerializer(
+            data=yaml_data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return serializer.errors
