b060db88d8f7d10516075ecf1f4779f98643a9c4
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2020-02-06 20:23:23 +0530

    Set debug to false

--- django-cloudlaunch/cloudlaunchserver/settings.py
+++ django-cloudlaunch/cloudlaunchserver/settings.py
@@ -27,7 +27,7 @@ SITE_ID = 1
 SECRET_KEY = 'CHANGEthisONinstall'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
