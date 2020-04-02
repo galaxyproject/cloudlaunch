47f93592e45d6496b4676fa4b97151e5d39a8e62
Alexandru Mahmoud <amahmou4@jhu.edu>
2020-04-02 17:36:38 -0400

    Adding env variables for chart version and repo

--- django-cloudlaunch/cloudlaunch/backend_plugins/cloudman2_app.py
+++ django-cloudlaunch/cloudlaunch/backend_plugins/cloudman2_app.py
@@ -453,6 +453,10 @@ class CloudMan2AnsibleAppConfigurer(AnsibleAppConfigurer):
                                default_flow_style=False,
                                allow_unicode=True).encode('utf-8'))
                 .decode('utf-8'),
+            'CM_CHART_VERSION': str(app_config.get('config_cloudman2', {})
+                .get('cm_chart_version', '')),
+            'CM_CHARTS_REPO': str(app_config.get('config_cloudman2', {})
+                .get('cm_charts_repo', '')),
         }
         if app_config.get('config_cloudman2', {}).get('cm_helm_values'):
             cm_docker_env.update({
